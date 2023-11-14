ad462fdf32dc239b20c0f86dda3cd5137a207bbe
Nico Casiean <casiean2@gmail.com>
2023-11-14 20:24:04 +0100

    Subiendo LastSong Componente creado con su respectiva data y tambien he creado la data para los componentes rrss

---
+++ src/data/Rrss.mock.ts
@@ -0,0 +1,34 @@
+export const Rrss = [
+    {
+        Rs: 'Facebook',
+        url: 'https://www.facebook.com/anthrboy/'
+    },
+    {
+        Rs: 'YouTube',
+        url: 'https://www.youtube.com/c/ANTHRB%C3%98Y'
+    },
+    {
+        Rs: 'Apple Music',
+        url: 'https://music.apple.com/es/artist/anthr-b%C3%B8y/1447314835'
+    },
+    {
+        Rs: 'Spotify',
+        url: 'https://open.spotify.com/intl-es/artist/6X1cRyD27j6YviZ28d67VK'
+    },
+    {
+        Rs: 'Discord',
+        url: 'https://discord.com/invite/hmQpbfFmPy'
+    },
+    {
+        Rs: 'Instagram',
+        url: 'https://www.instagram.com/anthrboy/'
+    },
+    {
+        Rs: 'Twitter',
+        url: 'https://twitter.com/anthrboy'
+    },
+    {
+        Rs: 'TikTok',
+        url: 'https://www.tiktok.com/@anthrboy'
+    },
+];
