cde77759af45a5a32c6f70ba4c34ba21e747e1c2
Barbara Contardi <barbi.contardi@gmail.com>
2023-11-16 10:03:17 +0100

    refactor: button

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./style/App.scss";
 import Dev from "./pages/Dev";
 import { HomePage } from "./pages/home/HomePage";
-import { HomePageData } from '../src/data/HomePageData.mock'
-
-
-
+// import { HomePageData } from '../src/data/HomePageData.mock'
 
 function App() {
-
-
   return (
     <div className="Principal">
-    <Routes>
-    <Route path="/" element={<HomePage />} />
-      <Route path="dev" element={ <Dev /> } />
-
-    </Routes>
-  </div>
-  )
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="dev" element={<Dev />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
